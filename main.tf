c6787385ac95034c9a3b9ba8cdec2bcbcb4bd60b
jjeon17 <33076677+Jonathan940110@users.noreply.github.com>
2022-09-28 14:59:44 +0900

    create new subnet(BEFORE Subnet should be deleted)

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
-# terraform {
-#   cloud {
-#     organization = "jeonjiwan"
+terraform {
+  cloud {
+    organization = "jeonjiwan"
 
-#     workspaces {
-#       name = "terraform-cloud-test-dev"
-#     }
-#   }
-# }
+    workspaces {
+      name = "terraform-cloud-test-AFTER"
+    }
+  }
+}
 provider "aws" {
   region = "ap-northeast-2"
   access_key = "AKIA5T5J5373TD2GK5WG"
   secret_key = "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"
 }
 
-resource "aws_subnet" "subnet-1" {
+resource "aws_subnet" "subnet-2" {
   vpc_id = "vpc-05397ee1bcd1b5f72"
-  cidr_block = "10.10.8.0/24"
+  cidr_block = "10.10.9.0/24"
   availability_zone =  "ap-northeast-2a"
 
   tags = {
-    Name = "(BEFORE)Terraform-Cloud-Test-1"
+    Name = "(AFTER)Terraform-Cloud-Test-1"
   }
 }
